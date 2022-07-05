899b05d166091402639f69e72f7ba25d275c07b6
Paolo D'Alessandro <paolodalex96@gmail.com>
2022-07-05 10:58:59 +0200

    fix: reset to init files

--- src/main/java/it/itsrizzoli/auctive/AuctionApplication.java
+++ src/main/java/it/itsrizzoli/auctive/AuctionApplication.java
@@ -10,49 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuctionApplication implements CommandLineRunner {
-	/*
-	@Autowired
-    //private UserDao userRepository;
-
-	@Autowired
-	private ProductDao productRepository;
-    
-	@Autowired
-	private CartDao cartRepository;
-	*/
-	
 	public static void main(String[] args) {
 		SpringApplication.run(AuctionApplication.class, args);
 	}
 
     @Override
     public void run(String... args) {
-        /*Product p1 = productRepository.findById(2);
-        Product p2 = productRepository.findById(3);
 
-        User u = userRepository.findById(4);
-
-        Cart c = new Cart();
-        c.setCliente(u);
-        c.setAcquistoData(new Date());
-        c.setProdotto(p1);
-        cartRepository.save(c);
-
-        Cart c1 = new Cart();
-        c1.setCliente(u);
-        c1.setAcquistoData(new Date());
-        c1.setProdotto(p2);
-        cartRepository.save(c1);*/
-
-    	/*List<Cart> carts = cartRepository.findByClienteId(4L);
-    	System.out.print("");
-
-    	User u2 = userRepository.findById(2L);
-    	carts.get(0).setCliente(u2);
-
-    	cartRepository.save(carts.get(0));
-
-
-    	prova commit Jimwell*/
     }
 }
