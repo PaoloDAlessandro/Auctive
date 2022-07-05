7b13c41b0178889734dad963f47298befc78f40a
Robert Hanu <r.hanu@itsrizzoli.it>
2022-07-05 17:33:37 +0200

    feat: added validation

--- src/main/java/it/itsrizzoli/auctive/model/Research.java
+++ src/main/java/it/itsrizzoli/auctive/model/Research.java
@@ -1,7 +1,12 @@
 package it.itsrizzoli.auctive.model;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class Research {
 
+    @NotNull(message = "Quest campo è obbligatorio")
+    @Min(2)
     private String research;
 
     public Research(String research) {
