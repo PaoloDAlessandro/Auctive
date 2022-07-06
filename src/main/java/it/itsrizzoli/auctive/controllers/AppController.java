c6561cd0604b38265b96e3bbf4a2224c4a19e9a0
Robert Hanu <r.hanu@itsrizzoli.it>
2022-07-06 20:15:36 +0200

    feat: implemented controller for products

--- src/main/java/it/itsrizzoli/auctive/controllers/AppController.java
+++ src/main/java/it/itsrizzoli/auctive/controllers/AppController.java
@@ -5,7 +5,11 @@ import it.itsrizzoli.auctive.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -19,5 +23,37 @@ public class AppController {
         List<Product> listProducts = service.listAll();
         model.addAttribute("listProducts", listProducts);
         return "research-results";
+    }
+
+    @RequestMapping("/new")
+    public String showNewProductForm(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "new-product";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product") Product product) {
+        service.save(product);
+
+        return  "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditProductForm(@PathVariable(name = "id") int id) {
+        ModelAndView mav = new ModelAndView("edit-product");
+
+        Product product = service.get(id);
+        mav.addObject("product", product);
+
+        return mav;
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable(name = "id") int id) {
+
+        service.delete(id);
+
+        return "redirect:/";
     }
 }
