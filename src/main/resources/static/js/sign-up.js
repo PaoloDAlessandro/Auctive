2eefd5e6d31cbaf998490123a41d842078366e8c
AstrallTrinity <101412297+AstrallTrinity@users.noreply.github.com>
2022-07-08 17:34:48 +0200

    feat: insert arrow for return the display in sign-up

--- src/main/resources/static/js/sign-up.js
+++ src/main/resources/static/js/sign-up.js
@@ -1,4 +1,4 @@
- //MOBILE SCRIPT - PAOLO
+
 
 const next_button = document.getElementById("next");
 
@@ -13,4 +13,23 @@ next_button.addEventListener("click", function(){
     document.getElementById("address").style.display="none";
     document.getElementById("next").style.display="none";
     document.getElementById("submit").style.display="block";
+    document.getElementById("arrow").style.display="block";
 });
+
+const arrow_back = document.getElementById("arrow");
+
+arrow_back.addEventListener("click", function(){
+    document.getElementById("name").style.display="block";
+    document.getElementById("surname").style.display="block";
+    document.getElementById("email").style.display="block";
+    document.getElementById("username").style.display="none";
+    document.getElementById("birthdate").style.display="none";
+    document.getElementById("password").style.display="none";
+    document.getElementById("confirm_password").style.display="none";
+    document.getElementById("address").style.display="block";
+    document.getElementById("next").style.display="block";
+    document.getElementById("submit").style.display="none";
+    document.getElementById("arrow").style.display="none";
+
+});
+
