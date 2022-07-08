3cf4cae9dfcb60802da70985101e0076ef7b7547
jimwellf <jimwellfulgencio@gmail.com>
2022-07-08 15:52:57 +0200

    feat: profile implemantation dynamic data

--- src/main/java/it/itsrizzoli/auctive/controllers/AccountController.java
+++ src/main/java/it/itsrizzoli/auctive/controllers/AccountController.java
@@ -1,25 +1,50 @@
 package it.itsrizzoli.auctive.controllers;
-
+import it.itsrizzoli.auctive.ProductService;
+import it.itsrizzoli.auctive.ProfileService;
+import it.itsrizzoli.auctive.dao.ProfileRepository;
 import it.itsrizzoli.auctive.dao.UserRepository;
 import it.itsrizzoli.auctive.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping(value = "/profile")
 public class AccountController {
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
-    UserRepository userRepository;
+    private ProfileService service;
 
-    @GetMapping("/profile")
-    public String getProfile(User user, HttpSession session) {
-        //user = (User) session.getAttribute("userLogged");
+    @GetMapping("")
+    public String getProfile(Model model, User user, HttpSession session) {
         user = userRepository.findByEmailUser((String) session.getAttribute("userLogged"));
-        System.out.println(user.getEmailUser());
+        model.addAttribute("user", user);
         return "profile";
     }
-}
 
+    @GetMapping(path = "/edit")
+    public String getProfileEdit(User user, HttpSession session) {
+        //User userLogged = (User) session.getAttribute("userLogged");
+        return "profile-edit.html";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditProfileForm(@PathVariable(name = "id") int id) {
+        ModelAndView mav = new ModelAndView("profile-edit");
+
+        User user = service.get(id);
+        mav.addObject("user", user);
+
+        return mav;
+    }
+
+}
