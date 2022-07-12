9fce4c4343a71cc25f2ba33922f243a09cc5f8e5
Marco Stefani <m.stefani@itsrizzoli.it>
2022-07-12 09:23:30 +0200

    feat: da modificare

--- src/main/java/it/itsrizzoli/auctive/controllers/HomepageController.java
+++ src/main/java/it/itsrizzoli/auctive/controllers/HomepageController.java
@@ -7,6 +7,7 @@ import it.itsrizzoli.auctive.model.ProductUser;
 import it.itsrizzoli.auctive.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,22 +20,17 @@ import java.util.List;
 
 @Controller
 public class HomepageController {
-    
+
     @Autowired
     UserRepository userRepository;
 
     @GetMapping("/")
-    public String getHomepage(HttpSession session) {
+    public String getHomepage(Model model, User user, HttpSession session) {
+        user = userRepository.findByEmailUser((String) session.getAttribute("userLogged"));
+        System.out.println(user);
+        model.addAttribute("user", user);
 
-        User user = userRepository.findByEmailUser((String) session.getAttribute("userLogged"));
-        ModelAndView modelAndView = new ModelAndView();
-        if (user != null){
-            modelAndView.addObject("user", user);
-            return "/index.html";
-        } else {
-            return "/index.html";
-        }
-
+        return "/index";
     }
 
 }
